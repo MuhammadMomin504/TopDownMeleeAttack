509b847a66f0c1022c84bf91a0cca9e940761890
MuhammadMomin504 <momin.1867@gmail.com>
2023-12-22 01:50:56 +0500

    implement player movement (WIP)

--- TopDownMeleeAttackProject/Assets/Scripts/PlayerController.cs
+++ TopDownMeleeAttackProject/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MovementController
 {
+    #region Private_Variables
+
+    private Vector3 inputVector = default;
+    private float rightFloat = 0f;
+    private float forwardFloat = 0f;
+    
+   
+
+    #endregion
+    
+    #region Exposed_Variables
+
+    [SerializeField] private float movementSpeed = 5f;
+    
+
+    #endregion
+    
+    #region Getters
+
+    #endregion
+
+
+    private void Awake()
+    {
+        base.Awake();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +42,43 @@ public class PlayerController : MovementController
     void Update()
     {
         base.Update();
+
+        if (RightInput)
+        {
+            rightFloat = 1f;
+            //inputVector = new Vector3(1f, 0f, 0f);
+        }
+        if (LeftInput)
+        {
+            rightFloat = -1f;
+            //inputVector = new Vector3(-1f, 0f, 0f);
+        }
+        if (ForwardInput)
+        {
+            forwardFloat = 1f;
+            //inputVector = new Vector3(0f, 0f, 1f);
+        }
+        if (BackwardInput)
+        {
+            forwardFloat = -1f;
+            //inputVector = new Vector3(0f, 0f, -1f);
+        }
+
+        
+        if (!RightInput && !LeftInput && !ForwardInput && !BackwardInput)
+        {
+            rightFloat = 0f;
+            forwardFloat = 0f;
+        }
+        inputVector = new Vector3(rightFloat, 0f , forwardFloat);
+        //Debug.Log("Input vector = " + inputVector);
+
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MyRigidBody.MovePosition(transform.position + inputVector * Time.deltaTime * movementSpeed);
     }
 }
