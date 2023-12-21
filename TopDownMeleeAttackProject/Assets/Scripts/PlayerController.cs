ad548d2009ad37a75df50a8583286067fb5c0842
MuhammadMomin504 <momin.1867@gmail.com>
2023-12-21 23:54:39 +0500

    Implement input system and accessing it's variables using getters

--- TopDownMeleeAttackProject/Assets/Scripts/PlayerController.cs
+++ TopDownMeleeAttackProject/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MovementController
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        base.Update();
     }
 }
