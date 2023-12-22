439b9779977e51aea459d04edfafa0f7a9e1c343
MuhammadMomin504 <momin.1867@gmail.com>
2023-12-23 00:59:02 +0500

    update git ignore file

---
+++ TopDownMeleeAttackProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static bool left = false;
+    public static bool right = false;
+    public static bool jump = false;
+    public static bool backward = false;
+    public static bool forward = false;
+    
+    
+    private bool leftButton = false;
+    private bool rightButton = false;
+    private bool jumpButton = false;
+    private bool backwardButton = false;
+    private bool forwardButton = false;
+
+
+    public float leftFloat = 0f;
+    private float rightFloat = 0f;
+    private float jumpFloat = 0f;
+    private float backwardFloat = 0f;
+    private float forwardFloat = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) )
+        {
+            rightButton = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) )
+        {
+            leftButton = true;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) )
+        {
+            forwardButton = true;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) )
+        {
+            backwardButton = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpButton = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            rightButton = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            leftButton = false;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            forwardButton = false;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            backwardButton = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpButton = false;
+        }
+        
+        if (leftButton)
+            leftFloat = 1.0f;
+        if (rightButton)
+            rightFloat = 1.0f;
+        if (jumpButton)
+            jumpFloat = 1.0f;
+        if (backwardButton)
+            backwardFloat = 1.0f;
+        if (forwardButton)
+            forwardFloat = 1.0f;
+        
+        left = leftFloat > 0.05f;
+        right = rightFloat > 0.05f;
+        jump = jumpFloat > 0.05f;
+        backward = backwardFloat > 0.05f;
+        forward = forwardFloat > 0.05f;
+
+    }
+    
+    void LateUpdate()
+    {
+        //Reset values to zero
+        leftFloat = Mathf.MoveTowards(leftFloat, 0.0f, Time.deltaTime * 25.0f);
+        rightFloat = Mathf.MoveTowards(rightFloat, 0.0f, Time.deltaTime * 25.0f);
+        jumpFloat = Mathf.MoveTowards(jumpFloat, 0.0f, Time.deltaTime * 25.0f);
+        backwardFloat = Mathf.MoveTowards(backwardFloat, 0.0f, Time.deltaTime * 25.0f);
+        forwardFloat = Mathf.MoveTowards(forwardFloat, 0.0f, Time.deltaTime * 25.0f);
+
+        if (!Input.anyKey)
+        {
+            leftButton = false;
+            rightButton = false;
+            forwardButton = false;
+            backwardButton = false;
+            jumpButton = false;
+        }
+       
+    }
+}
---
+++ TopDownMeleeAttackProject/Assets/Scripts/MovementController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    #region Private_Variables
+
+    private bool leftInput = false;
+    private bool rightInput = false;
+    private bool forwardInput = false;
+    private bool backwardInput = false;
+    private bool jumpInput = false;
+
+    private Rigidbody myRigidBody = default;
+    
+    #endregion
+
+    #region Exposed_Variables
+
+    
+
+    #endregion
+
+    #region Getters
+
+    public bool LeftInput => leftInput;
+    public bool RightInput => rightInput;
+    public bool ForwardInput => forwardInput;
+    public bool BackwardInput => backwardInput;
+    public bool JumpInput => jumpInput;
+    public Rigidbody MyRigidBody => myRigidBody;
+
+    #endregion
+
+    public void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        leftInput = InputManager.left;
+        rightInput = InputManager.right;
+        forwardInput = InputManager.forward;
+        backwardInput = InputManager.backward;
+    }
+}
